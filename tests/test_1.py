398aada48c27ecef0393b64a184cd19dd730d9db
Tevž Selčan <tevz.selcan@gmail.com>
2026-04-19 10:17:12 +0200

    Drug test

--- tests/test_1.py
+++ tests/test_1.py
@@ -2,6 +2,7 @@ def test_basic_math():
     assert 2 + 2 == 4
 
 
-def test_main_exists():
-    import main
-    assert hasattr(main, "__name__")
+
+def test_string():
+    text = "CI/CD"
+    assert text.lower() == "ci/cd"
